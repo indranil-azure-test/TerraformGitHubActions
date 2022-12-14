9d954073cef96f2de3266352e748e5f02fbc3a89
Indra <indranil_ranu@hotmail.com>
2022-12-14 23:29:37 +0530

    Trying TF Destroy

--- AzTerraformGIT/my_test_modules/test_rg_deploy.tf
+++
@@ -1,9 +0,0 @@
-
-resource "azurerm_resource_group" "RG" {
-  name     = "test_rg_module_ranu"
-  location = "Central India"
-
-tags = {
-  Owner = "Indranil Ranu"
-}
-}
--- AzTerraformGIT/my_test_modules/test_vnet_deploy.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_virtual_network" "vnet" {
-  name                = "test_vnet_module_ranu"
-  resource_group_name = azurerm_resource_group.RG.name
-  location            = azurerm_resource_group.RG.location
-  address_space       = ["10.0.0.0/16"]
-}
