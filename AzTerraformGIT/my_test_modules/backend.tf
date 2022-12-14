2c57ec06638463c550ce9a23a6c4c8b1fa5c8b8d
Indra <indranil_ranu@hotmail.com>
2022-12-14 17:51:45 +0530

    Apply Stage workflow updated

---
+++ AzTerraformGIT/my_test_modules/backend.tf
@@ -0,0 +1,6 @@
+backend "azurerm" {
+  resource_group_name   = "TF-GIT-CICD"
+  storage_account_name  = "terrformcodegit"
+  container_name        = "tfstatefile"
+  key                   = "demo.terraform.tfstate"
+  }
