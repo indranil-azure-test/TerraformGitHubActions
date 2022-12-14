7ccddb1768e563c44f5256f4bd2c63edfd2465c1
Indra <indranil_ranu@hotmail.com>
2022-12-14 18:04:42 +0530

    Apply Backend

--- AzTerraformGIT/my_test_modules/backend.tf
+++ AzTerraformGIT/my_test_modules/backend.tf
@@ -1,6 +1,10 @@
-backend "azurerm" {
-  resource_group_name   = "TF-GIT-CICD"
-  storage_account_name  = "terrformcodegit"
-  container_name        = "tfstatefile"
-  key                   = "demo.terraform.tfstate"
-  }
+
+terraform {
+    backend "azurerm" {
+        resource_group_name   = "TF-GIT-CICD"
+        storage_account_name  = "terrformcodegit"
+        container_name        = "tfstatefile"
+        key                   = "demo.terraform.tfstate"
+   }
+}
+
