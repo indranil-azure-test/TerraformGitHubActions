9207820238862a45ebf95714d729ff8cd043a824
Indra <indranil_ranu@hotmail.com>
2022-12-14 21:19:44 +0530

    YML FILE REFRESH

--- AzTerraformGIT/my_test_modules/backend.tf
+++ AzTerraformGIT/my_test_modules/backend.tf
@@ -4,7 +4,7 @@ terraform {
         resource_group_name   = "TF-GIT-CICD"
         storage_account_name  = "terrformcodegit"
         container_name        = "tfstatefile"
-        key                   = "demo.terraform.tfstate"
+        key                   = "terraform.tfstate"
    }
 }
 
