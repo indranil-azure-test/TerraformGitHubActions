d7c01525816baca466016e7f77b2ddd5e3752b5b
Indra <indranil_ranu@hotmail.com>
2022-12-14 21:36:34 +0530

    YML FILE REFRESH

--- AzTerraformGIT/main.tf
+++ AzTerraformGIT/main.tf
@@ -2,3 +2,13 @@ module "test_module" {
   source = "./my_test_modules"
 }
 
+
+terraform {
+    backend "azurerm" {
+        resource_group_name   = "TF-GIT-CICD"
+        storage_account_name  = "terrformcodegit"
+        container_name        = "tfstatefile"
+        key                   = "terraform.tfstate"
+   }
+}
+
